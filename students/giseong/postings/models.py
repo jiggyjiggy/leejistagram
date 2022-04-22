360af6a1edf4b378e0ea573149f5a7d69d3ec3d0
jiggy <jiggyjiggy0323@gmail.com>
2022-04-22 17:31:13 +0900

    feat: Add comment model

--- students/giseong/postings/models.py
+++ students/giseong/postings/models.py
@@ -11,7 +11,17 @@ class Posting(models.Model):
 
 class Image(models.Model):
     image_url = models.URLField(max_length=500)
-    posting   = models.ForeignKey('Posting', on_delete=models.CASCADE, null=True)
+    posting   = models.ForeignKey('Posting', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'images'
+
+class Comment(models.Model):
+    comment    = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    posting    = models.ForeignKey('Posting', on_delete=models.CASCADE, null=True)
+    user       = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'comments'

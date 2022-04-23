19f99a1f84327a6ff986aeb446f85aed29243015
jiggy <jiggyjiggy0323@gmail.com>
2022-04-23 16:40:09 +0900

    feat: Add like model and like view

--- giseong/postings/urls.py
+++ giseong/postings/urls.py
@@ -1,8 +1,13 @@
 from django.urls    import path
 
-from postings.views import PostingView, CommentView
+from postings.views import (
+                        PostingView,
+                        CommentView,
+                        LikeView
+                    )
 
 urlpatterns = [
     path('/posting', PostingView.as_view()),
-    path('/comment', CommentView.as_view())
+    path('/comment', CommentView.as_view()),
+    path('/like', LikeView.as_view())
 ]
